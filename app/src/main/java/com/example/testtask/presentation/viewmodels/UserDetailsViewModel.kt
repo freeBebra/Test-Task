103a562c08a346357a9c37bfac44807887ad38f2
Evgeny <shpagin.j@gmail.com>
2024-01-31 20:52:07 +0700

    feat: add links for email, address, coordinates, phone numbers

--- app/src/main/java/com/example/testtask/presentation/viewmodels/UserDetailsViewModel.kt
+++ app/src/main/java/com/example/testtask/presentation/viewmodels/UserDetailsViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.testtask.presentation.viewmodels
 
+import android.content.Intent
+import android.net.Uri
 import android.view.View
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -10,6 +12,8 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import java.util.Locale
+
 
 class UserDetailsViewModel(
     private val userId: Int,
@@ -29,5 +33,20 @@ class UserDetailsViewModel(
     fun onNavigationClick(view: View) {
         val navController = view.findNavController()
         navController.popBackStack()
+    }
+
+    fun onCoordinatesClick(view: View) {
+        val coordinates = user.value!!.location.coordinates
+        val uri =
+            String.format(Locale.ENGLISH, "geo:%f,%f", coordinates.latitude, coordinates.longitude)
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+        view.context.startActivity(intent)
+    }
+
+    fun onAddressClick(view: View) {
+        val address = user.value!!.location.address.toString()
+        val uri = String.format(Locale.ENGLISH, "geo:0,0?q=${address}")
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+        view.context.startActivity(intent)
     }
 }
